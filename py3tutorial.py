515a4c4af4f10f5cadf691519475540691e7f9f3
ravi teja <rtejaragi@gmail.com>
2023-02-27 17:30:51 -0500

    initialcommit

---
+++ py3tutorial.py
@@ -0,0 +1,10 @@
+print('Hello World')
+
+programming_languages = "Python" , "Java" ,"GoLang" , "c" 
+
+print(type(programming_languages))
+
+
+for language  in programming_languages: 
+    print(language)
+
